7ae30368556a3ea397efe6dd46a9f9fd3d998114
Kristina <kristina.palmquist@gmail.com>
2025-05-23 15:53:32 +0200

    deleted course project

--- Classes-and-Object-oriented-Programming-in-Python-3/module5.py
+++ Classes-and-Object-oriented-Programming-in-Python-3/module5.py
@@ -2,44 +2,57 @@
 """ Implementing Class Inheritance"""
 
 # DRY - Don't Repeat Yourself
-class Person: # base class / parent class / superclass
+
+
+class Person:  # base class / parent class / superclass
     def __init__(self, name, age):
         self.name = name
         self.age = age
+        
     def project_update(self):
         pass
-    
-class Client(Person): # derived class / child class / subclass
+
+
+class Client(Person):  # derived class / child class / subclass
     def __init__(self, name, age):
         super().__init__(name, age)
+
 
 class Employee(Person):
     def __init__(self, name, age, salary):
         super().__init__(name, age)
         self.salary = salary
+
     def __str__(self):
-        return f'{self.name} is {self.age} years old. Employee has a salary of ${self.salary}'
+        return (f"""{self.name} is {self.age} years old.
+                Employee has a salary of ${self.salary}""")
+
     def increase_salary(self, percent):
         self.salary += self.salary * (percent/100)
-        
+
+
 class Developer(Employee):
     def __init__(self, name, age, salary, framework):
         self.framework = framework
         super().__init__(name, age, salary)
-    def increase_salary(self, percent, bonus=0): # method overriding
+        
+    def increase_salary(self, percent, bonus=0):  # method overriding
         # self.salary += self.salary * (percent/100)
-        super().increase_salary(percent) # dynamic
-        # Employee.increase_salary(self, percent) # method resolution order, will have to be changed if hierarchy changes
+        super().increase_salary(percent)  # dynamic
+        # Employee.increase_salary(self, percent) # method resolution order,
+        # will have to be changed if hierarchy changes
         self.salary += bonus
         # return super().increase_salary(percent)
-        
+
+
 class Tester(Employee):
     def run_tests(self):
         print(f'Testing is started by {self.name}...')
         print('Tests are done')
 
+
 employee1 = Tester(name='Lauren', age=44, salary=1000)
-employee2 = Developer(name='Ji-Soo', age=38, salary=1000, framework="Flask") 
+employee2 = Developer(name='Ji-Soo', age=38, salary=1000, framework="Flask")
 print(employee2.name)
 print(employee2.framework)
 
